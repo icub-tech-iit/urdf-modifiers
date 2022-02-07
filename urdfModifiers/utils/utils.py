d25f722232254c989a9974cffd465194cbfb61a5
GrmanRodriguez <grmanrod@gmail.com>
2022-02-07 15:31:39 +0100

    added Modification class and updated other classes to support it

--- urdfModifiers/utils/utils.py
+++ urdfModifiers/utils/utils.py
@@ -3,8 +3,6 @@ from urdfpy import URDF
 from urdfModifiers.geometry import *
 import os
 
-from urdfModifiers.geometry.geometry import Modification
-
 def write_urdf_to_file(urdf, filename, gazebo_plugins):
     """Saves the URDF to a valid .urdf file, also adding the gazebo_plugins"""
     urdf.save(filename)
@@ -40,35 +38,6 @@ def create_dummy_file(dummy_filename, text):
     """Creates a dummy file to save information"""
     with open(dummy_filename, 'w') as f:
         f.writelines(text)
-
-def parse_modifications(config_section):
-    """Returns the modifications from a section of the ini file"""
-    modifications = {}
-    dimension_scale = config_section.get(geometry.Modification.DIMENSION+geometry.Modification.SCALE, None)
-    if dimension_scale is not None:
-        modifications[geometry.Modification.DIMENSION] = [float(dimension_scale), geometry.Modification.MULTIPLIER]
-    density_scale = config_section.get(geometry.Modification.DENSITY+geometry.Modification.SCALE, None)
-    if density_scale is not None:
-        modifications[geometry.Modification.DENSITY] = [float(density_scale), geometry.Modification.MULTIPLIER]
-    radius_scale = config_section.get(geometry.Modification.DENSITY+Modification.SCALE, None)
-    if radius_scale is not None:
-        modifications[geometry.Modification.RADIUS] = [float(radius_scale), geometry.Modification.MULTIPLIER]
-    mass_scale = config_section.get(geometry.Modification.MASS+geometry.Modification.SCALE, None)
-    if mass_scale is not None:
-        modifications[geometry.Modification.MASS] = [float(mass_scale), geometry.Modification.MULTIPLIER]
-    dimension = config_section.get(geometry.Modification.DIMENSION, None)
-    if dimension is not None:
-        modifications[geometry.Modification.DIMENSION] = [float(dimension), geometry.Modification.ABSOLUTE]
-    density = config_section.get(geometry.Modification.DENSITY, None)
-    if density is not None:
-        modifications[geometry.Modification.DENSITY] = [float(density), geometry.Modification.ABSOLUTE]
-    radius = config_section.get(geometry.Modification.RADIUS, None)
-    if radius is not None:
-        modifications[geometry.Modification.RADIUS] = [float(radius), geometry.Modification.ABSOLUTE]
-    mass = config_section.get(geometry.Modification.MASS, None)
-    if mass is not None:
-        modifications[geometry.Modification.MASS] = [float(mass), geometry.Modification.ABSOLUTE]
-    return modifications
 
 def erase_dummy_file(dummy_filename):
     """Erases the dummy file"""
