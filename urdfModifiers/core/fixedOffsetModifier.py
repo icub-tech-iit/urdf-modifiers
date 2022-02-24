2374e2e6ea64d33edc23a1065b5a562def971bd2
AlexAntn <alexandre.antunes@plymouth.ac.uk>
2022-02-24 12:34:08 +0100

    added some documentation comments for the formulas

--- urdfModifiers/core/fixedOffsetModifier.py
+++ urdfModifiers/core/fixedOffsetModifier.py
@@ -11,7 +11,76 @@ from urdfModifiers.geometry.geometry import Geometry, Side
 
 @dataclass
 class FixedOffsetModifier():
-    """Class to modify link that is Z-parallel with its previous and following joints, while keeping the offsets"""
+    """
+    Class to modify link that is Z-parallel with its previous and following joints, while keeping the offsets
+    
+        --- +-----------+ -
+        ^   |           | ^
+    e_o |   |     ^     | |
+        |   |     |     | |
+        --- |     o-->  | |-----------
+            |   child   | |          ^
+            |           | | v_l      |
+            |           | |          |
+            |     o---- | |----      |
+            |   visual  | |   ^      | j_o
+            |           | |   |      |
+            |           | |   |      |
+            |           | |   |      |
+            |           | |   |      |
+            |           | |   |      |
+            |           | v   |      |
+        --- +-----------+ -   | v_o  |
+        ^         ^           |      |
+    s_o |         |           |      |
+        |         o-->        |      |
+                parent
+
+    Considering the above link, we define the following values:
+    s_o - distance between the link frame (the parent origin referential) and the start of the link visual element      - parent_joint_offset
+    e_o - distance between the child link frame (the child origin referential) and the end of the link visual element   - child_joint_offset
+
+    j_o - distance between the axis connecting the link to its parent and the axis connecting the link to its child     - child_joint_origin
+    v_l - length of the visual element (box or cylinder) of the link                                                    - link_length
+    v_o - distance between the link frame and the center of the visual element                                          - link_visual_origin
+
+    Using these definitions, we can relate them using the following formulas:
+
+    s_o = v_o - v_l * sign(j_o) / 2
+    e_o = v_o + v_l * sign(j_o) / 2 - j_o
+
+    Using these formulas, and assuming we want to keep the values s_o and e_o constant, a modification of the link length would lead 
+    to a known change of v_l, which we define as v_l'. Knowing v_l', s_o and e_o we compute the 2 remaining values, v_o' and j_o', therefore completely defining
+    the origin of both the link visual and the child joint:
+
+    v_o' = s_o + v_l' * sign(j_o) / 2
+    j_o' = v_o' - e_o + v_l' * sign(j_o) / 2
+
+    or, solving for v_o'
+
+    j_o' = s_o + v_l' * sign(j_o) - e_o
+
+    IMPORTANT NOTE:
+    This fixed offset modification only works for z-parallel links, that is, when the parent joint, link and child joint XY planes are all parallel:
+
+      /--^----/
+     /   |   /     child XY plane
+    /-------/
+
+        /--^----/
+       /   |   /   link XY plane
+      /-------/
+
+      /--^----/
+     /   |   /     parent XY plane
+    /-------/
+
+    if the link you are changing is not z-parallel, you should instead use the API available in JointModifier and LinkModifier
+            
+    For more details regarding the calculation of these values, you can check the following link:
+    https://github.com/icub-tech-iit/urdf-modifiers/issues/11
+    
+    """
 
     def __init__(self, link, robot):
         self.link = link
@@ -85,12 +154,17 @@ class FixedOffsetModifier():
 
     def calculate_offsets(self):
         link_length = self.get_significant_length()
-        link_origin = self.get_link_origin(self.link)
-        link_origin_z = link_origin[2]
+        link_visual_origin = self.get_link_origin(self.link)
+        link_visual_origin_z = link_visual_origin[2]
         child_joint_origin = self.get_joint_origin(self.child_joint)
         child_joint_origin_z = (child_joint_origin[2] if child_joint_origin is not None else 0)
-        parent_joint_offset = (link_origin_z - link_length * math.copysign(1, child_joint_origin_z) / 2 if self.parent_joint else None)
-        child_joint_offset = (link_origin_z + link_length * math.copysign(1, child_joint_origin_z) / 2 - child_joint_origin_z if self.child_joint else None)
+
+        # Using formula: s_o = v_o - v_l * sign(j_o) / 2
+        parent_joint_offset = (link_visual_origin_z - link_length * math.copysign(1, child_joint_origin_z) / 2 if self.parent_joint else None)
+
+        # Using formula: e_o = v_o + v_l * sign(j_o) / 2 - j_o
+        child_joint_offset = (link_visual_origin_z + link_length * math.copysign(1, child_joint_origin_z) / 2 - child_joint_origin_z if self.child_joint else None)
+
         return parent_joint_offset, child_joint_offset
 
     def modify(self, modifications):
@@ -124,11 +198,13 @@ class FixedOffsetModifier():
         link_modification = Modification()
 
         if parent_joint_offset is not None:
+            # Using formula: v_o' = s_o + v_l' * sign(j_o) / 2 
             new_link_origin = parent_joint_offset + new_length * math.copysign(1, child_joint_origin_z) / 2
             link_modification.add_position(new_link_origin, absolute=True)
         else:
             # for the joint calculations, if there is no parent we position it as if it were in the center of the visual
-            parent_joint_offset = -new_length / 2
+            # s_o = v_o - v_l * sign(j_o) / 2   with  v_o = 0
+            parent_joint_offset = -new_length * math.copysign(1, child_joint_origin_z) / 2
 
         geometry_type, _ = self.get_geometry(self.link_modifier.get_visual())
         if geometry_type == Geometry.SPHERE:        
@@ -139,6 +215,7 @@ class FixedOffsetModifier():
         self.link_modifier.modify(link_modification)
 
         if (self.child_joint is not None):
+            # j_o' = s_o + v_l' * sign(j_o) - e_o
             new_child_joint_origin = new_length * math.copysign(1, child_joint_origin_z) + parent_joint_offset - child_joint_offset
             joint_modification = Modification()
 
